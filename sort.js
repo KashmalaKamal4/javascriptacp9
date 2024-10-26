6b007c762be7c57b91a2f3cd449a8295cfae23c1
KashmalaKamal4 <kashmalakamal4@gmail.com>
2024-10-26 08:52:13 -0700

    Add files via upload

---
+++ sort.js
@@ -0,0 +1,20 @@
+{
+    let numbers=[20, 1, 2, 4, 15, 14];
+    numbers.sort((a,b) => (a-b)); //ascending order
+    console.log(numbers)
+    }
+    {
+    let numbers=[20, 1, 2, 4, 15, 14];
+    numbers.sort((a,b) => (b-a)); //descending order
+    console.log(numbers)
+    }
+    {
+    let numbers=[20, 1, 2, 4, 15, 14];
+    numbers.reverse();
+    console.log(numbers)
+    }
+    {
+        let num = [20, 10, 15, 45, 33, 76, 12];
+      let doublenum = num.map(number => number * 2);
+      console.log(doublenum)
+      }
